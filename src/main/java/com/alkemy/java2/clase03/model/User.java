3c5834218b1c8a4ae2c674a6f2dddc78c0f34a63
Analia <test@gmail.com>
2025-05-12 14:51:01 -0300

    Api Restfull Notification

--- src/main/java/com/alkemy/java2/clase03/model/User.java
+++ src/main/java/com/alkemy/java2/clase03/model/User.java
@@ -21,7 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class User {
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
  @NotBlank (message = "Name is mandatory")
